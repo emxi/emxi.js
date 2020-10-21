e071608ed6ecf36f9fd31a71f725fdc8c9483460
Kai Lyons <62919224+KaiLikesLinux@users.noreply.github.com>
2020-10-21 16:35:25 -0600

    first media option

--- emxi.js
+++ emxi.js
@@ -122,7 +122,9 @@ const core = {
 }
 
 const media = {
-    // Coming Soon
+    addYouTubeVideo: function (videoURL, tag){
+        document.getElementById(tag).innerHTML = "<iframe width=\"560\" height=\"315\" src=\"https://www.youtube.com/embed/" + videoURL + "\" frameborder=\"0\" allow=\"accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture\" allowfullscreen></iframe>"
+    }
 }
 
 const page = {
